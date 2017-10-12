e196ab721df590e99f734eb7b2ff6b2221049fd4
Märt Bakhoff <mart.bakhoff@gmail.com>
2017-10-12 08:40:08 +0300

    chown workspace to runas user

--- main.cpp
+++ main.cpp
@@ -124,6 +124,13 @@ struct workspace {
                 fail("mkdir " + dir);
             }
         }
+        if (!cfg.runas.empty()) {
+            struct passwd *pw = getpwnam(cfg.runas.c_str());
+            if (pw == NULL)
+                fail("no such user: " + cfg.runas);
+            if (chown(root.c_str(), pw->pw_uid, pw->pw_gid))
+                fail("chown workspace");
+        }
     }
 
     workspace(const workspace &src) = delete;
